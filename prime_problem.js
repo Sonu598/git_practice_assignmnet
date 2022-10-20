b55922d7e5516de3b76b57fc73eee1289756c6aa
sonu598 <115460626+Sonu598@users.noreply.github.com>
2022-10-20 07:39:13 +0530

    Update prime_problem.js

---
+++ prime_problem.js
@@ -0,0 +1,13 @@
+//Find 13 is prime or not. If prime, print "PRIME". Else print "NOT PRIME". 
+
+let num = 13;
+let count = 0;
+for (a=1;a<num;a++) {
+    if (Num%a==0) {
+	Count++;
+  } if (count==2) {
+	console.log("Prime");
+  } else {
+	console.log("not Prime")
+  }
+}
